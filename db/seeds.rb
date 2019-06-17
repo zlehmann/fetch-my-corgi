3961ff68c2cc0cad2f6949bdd6e071a55a6dbf48
Zachary Lehmann <zlehmann@gmail.com>
2019-06-16 21:12:48 -0400

    Added test seeds to DB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Dog.create(name: 'Bryce', url: 'https://www.petfinder.com/dog/bryce-44700619/pa/narberth/new-leash-on-life-usa-pa825/', source_id: 'bryce-44700619')
+Shelter.create(name: 'New Leash on Life USA', state: 'PA', city: 'Narberth', url: 'https://www.petfinder.com/member/us/pa/narberth/new-leash-on-life-usa-pa825/', address: '')
