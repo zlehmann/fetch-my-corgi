b875afc6e5fe26ad56d4cf6b92d73ba84cf2b851
Zachary Lehmann <zlehmann@gmail.com>
2019-06-18 19:57:59 -0400

    Resolved dependency issues after adding redux and thunk

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 ReactDOM.render(
   <App />,
