3401816555075c0da03b2fbc58457b33557b016c
Zachary Lehmann <zlehmann@gmail.com>
2019-06-17 21:34:11 -0400

    Built index and show methods in Dogs controller.

--- app/controllers/api/dogs_controller.rb
+++ app/controllers/api/dogs_controller.rb
@@ -1,2 +1,11 @@
 class Api::DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+    render json: @dogs
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+    render json: @dog
+  end
 end
--- app/controllers/api/shelters_controller.rb
+++ app/controllers/api/shelters_controller.rb
@@ -1,2 +1,7 @@
 class Api::SheltersController < ApplicationController
+  def index
+  end
+
+  def show
+  end 
 end
