7e32ad9de01cc9d8bc1e32aa9d948941bc072a3c
Zachary Lehmann <zlehmann@gmail.com>
2019-06-20 10:25:07 -0400

    Built out doglist component.

--- client/src/actions/dogActions.js
+++ client/src/actions/dogActions.js
@@ -2,10 +2,10 @@ export default function fetchDogs() {
   return (dispatch) => {
     dispatch({type: 'LOADING_DOGS'});
     console.log('dispatched loading dogs')
-    return fetch(`/api/dogs`)
+    return fetch("/api/dogs")
     .then(response => response.json())
     .then(responseJSON => {
-      dispatch({type: "FETCH_DOGS", payload: responseJSON})
+       dispatch({type: "FETCH_DOGS", payload: responseJSON})
     })
   }
 }
--- client/src/components/DogList.js
+++ client/src/components/DogList.js
@@ -4,9 +4,14 @@ class DogList extends Component {
 
   render() {
     console.log('props: ', this.props.dogs)
+    const dogList = this.props.dogs.dogs.map((dog) =>
+        <li key={dog.id}>{dog.name}</li>
+      )
+
     return(
       <div>
         <h2>Dogs will get listed here.</h2>
+        <ul>{dogList}</ul>
       </div>
     )
   }
