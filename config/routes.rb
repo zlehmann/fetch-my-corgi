6029fe8869a7c6432f6712ecd4d86dc4e2559a49
Zachary Lehmann <zlehmann@gmail.com>
2019-08-03 13:16:29 -0400

    adding update_backend route to api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #get 'api/dogs/fetch', to: 'api/dogs#fetch'
 
   namespace :api do
+    get 'dogs/update_backend', to: 'dogs#update_backend'
     resources :dogs
     resources :shelters, only: [:index, :show]
   end
